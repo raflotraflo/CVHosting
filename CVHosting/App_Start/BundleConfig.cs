60b839bd60bc66273c5574047f594c07917861e8
Rafał Chodzidło <rchodzidlo@aiut.com.pl>
2016-09-22 23:04:56 +0100

    aktualizacja biblitek jQuery

--- CVHosting/App_Start/BundleConfig.cs
+++ CVHosting/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace CVHosting
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-1.11.0.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
