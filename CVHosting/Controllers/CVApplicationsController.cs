da126d96f4ffecaafdb3a5f95d534433855b39b7
Rafał Chodzidło <rchodzidlo@aiut.com.pl>
2016-09-22 15:35:24 +0200

    dodaie pagowania stron

--- CVHosting/Controllers/CVApplicationsController.cs
+++ CVHosting/Controllers/CVApplicationsController.cs
@@ -10,6 +10,7 @@ using Repository.Models;
 using Repository.IRepo;
 using Repository.Logging;
 using Repository.Models.Views;
+using PagedList;
 
 namespace CVHosting.Controllers
 {
@@ -29,12 +30,46 @@ namespace CVHosting.Controllers
         }
 
         // GET: CVApplications
-        public ActionResult Index()
+        public ActionResult Index(int? page, string sortOrder)
         {
+            int currentPage = page ?? 1;
+            int onPage = 4;
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DateTimeSort = String.IsNullOrEmpty(sortOrder) ? "DateTime" : "";
+            ViewBag.NameSort = sortOrder == "Name" ? "NameAsc" : "Name";
+            ViewBag.WorkplaceSort = sortOrder == "Workplace" ? "WorkplaceAsc" : "Workplace";
+          
+
             //var cVApplication = db.CVApplication.Include(c => c.Availability).Include(c => c.Place);
-            var cVApplication = _cvApplicationsRepo.GetAllCVs();
-            var test = cVApplication.ToList();
-            return View(cVApplication.ToList());
+            var cVApplicationQuery = _cvApplicationsRepo.GetAllCVs();
+
+            switch (sortOrder)
+            {
+                //case "DateTime":
+                //    cVApplicationQuery = cVApplicationQuery.OrderByDescending(s => s.DataDodania);
+                //    break;
+                case "DateTime":
+                    cVApplicationQuery = cVApplicationQuery.OrderBy(s => s.DataDodania);
+                    break;
+                case "Name":
+                    cVApplicationQuery = cVApplicationQuery.OrderByDescending(s => s.Name);
+                    break;
+                case "NameAsc":
+                    cVApplicationQuery = cVApplicationQuery.OrderBy(s => s.Name);
+                    break;
+                case "Workplace":
+                    cVApplicationQuery = cVApplicationQuery.OrderByDescending(s => s.Workplace);
+                    break;
+                case "WorkplaceAsc":
+                    cVApplicationQuery = cVApplicationQuery.OrderBy(s => s.Workplace);
+                    break;
+                default:    // id descending
+                    cVApplicationQuery = cVApplicationQuery.OrderByDescending(s => s.DataDodania);
+                    break;
+            }
+
+            return View(cVApplicationQuery.ToPagedList<CVApplication>(currentPage, onPage));
         }
 
         // GET: CVApplications/Details/5
